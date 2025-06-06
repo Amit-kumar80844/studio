60bd44dd40cf1f580c004feb37216ed6e8e628ca
Amit-kumar80844 <156497411+Amit-kumar80844@users.noreply.github.com>
2025-06-06 13:13:39 +0530

    google search seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="dark">
       <head>
+        <meta name="google-site-verification" content="racop2sRv689NDUgfTzx98JELwFD-SiGhLbz7b_EFOU" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet" />
